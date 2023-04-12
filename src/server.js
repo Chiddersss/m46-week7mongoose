d8c6250e7bd177f20ff4374a9296eee9f299cb9a
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-04-12 14:33:14 +0100

    add db connection

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+require("./db/connection");
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(5001, () => console.log("Server is running on port 5001"));
